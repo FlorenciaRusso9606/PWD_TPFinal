0bd710e0b779850dd7872456d7f1517116595065
Florencia Russo <florenciarusso96@gmail.com>
2024-11-15 17:19:24 -0300

    modificando

--- Vista/index.php
+++ Vista/index.php
@@ -12,62 +12,58 @@ $arrayProductos = $objProducto->buscar(NULL);
 <?php include_once "../Estructura/header.php"; ?>
 <link rel="stylesheet" href="Assets/css/style.css">
 
-
 <div class="container mb-5">
   <h2 class="mt-5">Tienda Lenny</h2>
-  <h4 class=""> El Ateneo lo que más te gusta de Libros Físicos con las mejores ofertas.</h4>
+  <h4>El Ateneo - lo que más te gusta de Libros Físicos con las mejores ofertas.</h4>
   <p>¡Encontrá promociones y descuentos en toda la tienda!</p>
 </div>
 
+<div id="seccion-productos" class="container productos d-flex flex-wrap" >
 
-<div id="seccion-productos" class="container productos d-flex flex-wrap">
+  <?php if ($rolActual == 2) { ?>
+    <div class="sombra-caja border item-box m-5 d-flex flex-column align-items-center justify-content-center" style="min-width: 282px; min-height: 580px">
+      <h5 class="mb-5">Agregar Libro</h5>
+      <a href="nuevoLibro.php">
+        <i class="bi bi-plus-circle" style="font-size: 8rem;"></i>
+      </a>
+    </div>
+  <?php } ?>
 
-  <div class="container d-flex flex-wrap justify-content-center">
-    <?php if ($rolActual == 2) { ?>
-      <div class="sombra-caja border item-box m-5 d-flex flex-column align-items-center justify-content-center" style="min-width: 282px; min-height: 580px">
-        <h5 class="mb-5">Agregar Libro</h5>
-        <a href="nuevoLibro.php">
-          <i class="bi bi-plus-circle" style="font-size: 8rem;"></i>
-        </a>
-      <?php } ?>
-      </div>
-      <?php
-      foreach ($arrayProductos as $producto) {
-          if (($producto->getProCantStock() >= 0)) {
-            $detallesProAct = $producto->getProDetalle();
-            $dirImgAct = $producto->getIdProducto();
-            $imgArtic ="{$ROOT}Vista/img/Productos/{$dirImgAct}"
-      ?>
-              <h3 class="p-3 text-center"><?= $producto->getProNombre() ?></h3>
-              <a href="./productoPag.php?id=<?= $producto->getIdProducto() ?>&nombrecel=<?= $producto->getProNombre() ?>">
-              
+  <?php
+  if (!empty($arrayProductos)) {
+    foreach ($arrayProductos as $producto) {
+      if ($producto->getProCantStock() >= 0) {
+        $detallesProAct = $producto->getProDetalle();
+        $dirImgAct = $producto->getIdProducto();
+        $imgArtic = "img/Productos/{$dirImgAct}.jpg";
+        ?>
+        <div class="d-flex flex-column align-items-center justify-content-center text-center m-3" style="min-width: 300px;">
+          <h3 class="p-3"><?= $producto->getProNombre() ?></h3>
+          
+          <!-- Verificar si la imagen existe antes de mostrarla -->
+          <a href="./productoPag.php?id=<?= $producto->getIdProducto() ?>&nombrelibro=<?= $producto->getProNombre() ?>">
+            <img src="<?= $imgArtic ?>" alt="<?= $detallesProAct ?>" style="max-width: 200px; max-height: 280px;">
+          </a>
 
-              <?php if ($producto->getProCantStock() == 0) { ?>
-                <p class='fw-bold text-danger'>Producto sin stock</span></p>
-              
+          <?php if ($producto->getProCantStock() == 0) { ?>
+            <p class='fw-bold text-danger'>Producto sin stock</p>
+            <p class="text-muted text-decoration-line-through">$<?= $producto->getProPrecio() ?></p>
+          <?php } else { ?>
+            <p class='fs-4'>$<?= $producto->getProPrecio() ?></p>
+          <?php } ?>
 
-                <p class="text-nowrap">
-                  <span class='text-muted text-decoration-line-through'>$<?=  $producto->getProPrecio()  ?></span>
-            
-                </p>
-
-              <?php } else { ?>
-                <p class='fs-4'>$<?= $producto->getProPrecio()  ?> </p>
-              <?php } ?>
-
-              <?php if ($rolActual == 2) { ?>
-                <div class="d-flex">
-                  <a href="./nuevoProducto.php?id=<?= $producto->getIdProducto() ?>" class="btn btn-primary mx-1 mb-2">Editar</a>
-                </div>
-              <?php } ?>
-
+          <?php if ($rolActual == 2) { ?>
+            <div class="d-flex">
+              <a href="./nuevoProducto.php?id=<?= $producto->getIdProducto() ?>" class="btn btn-primary mx-1 mb-2">Editar</a>
             </div>
-      <?php }}?>
-  </div>
+          <?php } ?>
+        </div>
+      <?php }
+    }
+  } else { ?>
+    <p class="text-center">No hay productos disponibles en este momento.</p>
+  <?php } ?>
 
 </div>
 
-<?php include_once "./includes/footer.php"; ?>
-
-
-
+<?php include_once "../Estructura/footer.php"; ?>
--- configuracion.php
+++ configuracion.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+/*session_start();
 header('Content-Type: text/html; charset=utf-8');
 header("Cache-Control: no-cache, must-revalidate");
 
@@ -26,3 +26,34 @@ $_SERVER['LOGIN'] = $LOGIN;
 $_SERVER['PRINCIPAL'] = $PRINCIPAL;
 $_SERVER['REGISTRARSE'] = $REGISTRARSE;
 $_SERVER['RUTAVISTA'] = $RUTAVISTA;
+
+*/
+
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+header("Cache-Control: no-cache, must-revalidate");
+
+// Configuración de la aplicación
+$PROYECTO = 'TPS/PWD_TPFinal/PWD_TPFinal';
+$ROOT = $_SERVER['DOCUMENT_ROOT'] . "/$PROYECTO/";
+
+$GLOBALS['ROOT'] = $ROOT;
+
+$page_title = "TP Final - Grupo 12 - PWD 2024";
+
+include_once($ROOT . 'util/funciones.php');
+
+// Definición de rutas
+$LOGIN = "http://" . $_SERVER['HTTP_HOST'] . "/$PROYECTO/vista/login.php";
+$PRINCIPAL = "http://" . $_SERVER['HTTP_HOST'] . "/$PROYECTO/index.php";
+$RUTAVISTA = "http://" . $_SERVER['HTTP_HOST'] . "/$PROYECTO/vista/";
+$REGISTRARSE = "http://" . $_SERVER['HTTP_HOST'] . "/$PROYECTO/vista/registrarse.php";
+$RUTANAV = $ROOT . 'Estructura/navbar.php';
+$rutalogo = "./img/";
+
+// Variables de entorno
+$GLOBALS['LOGIN'] = $LOGIN;
+$GLOBALS['PRINCIPAL'] = $PRINCIPAL;
+$GLOBALS['REGISTRARSE'] = $REGISTRARSE;
+$GLOBALS['RUTAVISTA'] = $RUTAVISTA;
+
