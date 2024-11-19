a342e712d4d41e39a82d5d2643b15f81b8c3a231
Rocio-Ayelen-Fernandez <rayelen.fernandez@est.fi.uncoma.edu.ar>
2024-11-19 09:05:20 -0300

    Agregar funcionalidades para gestionar menús: alta, edición y eliminación

--- Vista/Accion/edit_menu.php
+++ Vista/Accion/edit_menu.php
@@ -1,23 +1,19 @@
 <?php
-include_once "../../../configuracion.php";
+include_once "../../configuracion.php";
 $data = data_submitted();
 $respuesta = false;
-if (isset($data['idmenu'])){
+if (isset($data['idmenu'])) {
     $objC = new AbmMenu();
     $respuesta = $objC->modificacion($data);
-    
-    if (!$respuesta){
+
+    if (!$respuesta) {
 
         $sms_error = " La accion  MODIFICACION No pudo concretarse";
-        
-    }else $respuesta =true;
-    
+    } else $respuesta = true;
 }
 $retorno['respuesta'] = $respuesta;
-if (isset($mensaje)){
-    
-    $retorno['errorMsg']=$sms_error;
-    
+if (isset($mensaje)) {
+
+    $retorno['errorMsg'] = $sms_error;
 }
 echo json_encode($retorno);
-?>
