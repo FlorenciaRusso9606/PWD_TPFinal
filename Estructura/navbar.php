14fcced14bb78dd04718c38ee99d29568e1aa6d4
Rocio-Ayelen-Fernandez <rayelen.fernandez@est.fi.uncoma.edu.ar>
2024-11-18 23:51:35 -0300

    Refactor navbar.php para cerrar adecuadamente de la etiqueta <nav>

--- Estructura/navbar.php
+++ Estructura/navbar.php
@@ -4,9 +4,9 @@ $nav = new ControlNav();
 $menus = $nav->getSubMenus($rol);
 
 ?>
+<nav class="ui container">
+    <?php if ($rol > 0) { ?>
 
-<?php if ($rol > 0) { ?>
-    <nav class="ui container">
         <div class="sixteen wide column">
             <div class="ui menu">
                 <a href="index.php" class="item">
@@ -20,30 +20,31 @@ $menus = $nav->getSubMenus($rol);
                 <?php } ?>
             </div>
         </div>
-    </nav>
-<?php } else { ?>
-    <div class="sixteen wide column">
-        <div class="ui menu">
-            <a href="index.php" class="item">
-                Inicio
-            </a>
-            <a href="libros.php" class="item">
-                Libros
-            </a>
-            <a href="cuentos.php" class="item">
-                Cuentos
-            </a>
-            <a href="historietas.php" class="item">
-                Historietas
-            </a>
-            <a href="../Vista/login.php" class="item">
-                <i class="user icon"></i>
-                Iniciar Sesión
-            </a>
-            <a href="cerrarSesion.php" class="item">
-                <i class="sign out alternate icon"></i>
-                registrarse
-            </a>
+
+    <?php } else { ?>
+        <div class="sixteen wide column">
+            <div class="ui menu">
+                <a href="index.php" class="item">
+                    Inicio
+                </a>
+                <a href="libros.php" class="item">
+                    Libros
+                </a>
+                <a href="cuentos.php" class="item">
+                    Cuentos
+                </a>
+                <a href="historietas.php" class="item">
+                    Historietas
+                </a>
+                <a href="../Vista/login.php" class="item">
+                    <i class="user icon"></i>
+                    Iniciar Sesión
+                </a>
+                <a href="cerrarSesion.php" class="item">
+                    <i class="sign out alternate icon"></i>
+                    registrarse
+                </a>
+            </div>
         </div>
-    </div>
-<?php } ?>
+    <?php } ?>
+</nav>
