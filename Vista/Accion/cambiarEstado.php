8673c689a46a95fc7561781b93e9a450596bc669
Florencia Russo <florenciarusso96@gmail.com>
2024-11-20 11:59:10 -0300

    modificando la funcion cancelarCompra, modificando el archivo compraEstado, cambiarEstado y cancelarCompra

--- Vista/Accion/cambiarEstado.php
+++ Vista/Accion/cambiarEstado.php
@@ -1,12 +1,21 @@
 <?php
 include_once "../../configuracion.php";
+
 $data = data_submitted();
-$response = ["success" => false, "message" => "Error desconocido"];
-if($data['nuevoestado'] && $data['idcompra']){
-    $controlCompra = new ControlCompra;
-    if($controlCompra->cambiarEstado($data)){
-        $response = ["success" => true, "message" => "Estado de compra cambiado exitosamente"];
+$response = ["success" => false, "message" => "Error desconocido."];
+
+if (isset($data['nuevoestado']) && isset($data['idcompra'])) {
+    $controlCompra = new ControlCompra();
+    if ($controlCompra->cambiarEstado($data)) {
+        $response["success"] = true;
+        $response["message"] = "Estado cambiado exitosamente.";
     } else {
-      $response["message"] = "No se pudo cambiar el estado de compra";
+        $response["message"] = "No se pudo cambiar el estado.";
     }
+} else {
+    $response["message"] = "Faltan datos para cambiar el estado.";
 }
+
+// Enviar la respuesta en formato JSON
+header("Content-Type: application/json");
+echo json_encode($response);
