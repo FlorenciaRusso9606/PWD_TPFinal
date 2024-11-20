bfec7a2d0f5efd9255ccff85997ac8281a922ac3
Rocio-Ayelen-Fernandez <rayelen.fernandez@est.fi.uncoma.edu.ar>
2024-11-20 20:18:12 -0300

    Mejorar la respuesta JSON al crear un producto y corregir la estructura del código en ControlProducto y AbmProducto

--- Vista/Accion/accionNuevoProducto.php
+++ Vista/Accion/accionNuevoProducto.php
@@ -2,14 +2,19 @@
 include_once "../../configuracion.php";
 
 $controlProducto = new ControlProducto();
-$data =data_submitted();
+$data = data_submitted();
 
 // Realizar la acción según los datos
 $resultado = $controlProducto->realizarAccion($data);
 
+$response = array();
+if ($resultado) {
+    $response['success'] = true;
+    $response['message'] = 'Producto creado exitosamente.';
+} else {
+    $response['success'] = false;
+    $response['message'] = 'Hubo un error al crear el producto.';
+}
+
 header('Content-Type: application/json');
-echo json_encode([
-    'success' => $resultado,
-    'message' => $resultado ? 'Operación exitosa.' : 'No se pudo cambiar el estado.'
-]);
-exit;
+echo json_encode($response);
